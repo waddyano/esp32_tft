cebc3184e5c82ec619fec0b0cedd74dcc51e6201
prenticedavid <david.prentice@farming.co.uk>
2017-01-11 11:59:34 +0000

    update some SUPPORT sizes

--- MCUFRIEND_kbv.cpp
+++ MCUFRIEND_kbv.cpp
@@ -2,15 +2,15 @@
 #define SUPPORT_0154              //S6D0154 +320 bytes
 //#define SUPPORT_1289              //costs about 408 bytes
 #define SUPPORT_1963              //only works with 16BIT bus anyway
-//#define SUPPORT_4532              //LGDP4532 +120 bytes
+//#define SUPPORT_4532              //LGDP4532 +120 bytes.  thanks Leodino
 #define SUPPORT_4535              //LGDP4535 +180 bytes
 #define SUPPORT_68140             //RM68140 +52 bytes defaults to PIXFMT=0x55
 #define SUPPORT_7781              //ST7781 +172 bytes
 //#define SUPPORT_8347D             //HX8347-D, HX8347-G, HX8347-I +520 bytes, 0.27s
 //#define SUPPORT_8347A             //HX8347-A +500 bytes, 0.27s
 //#define SUPPORT_8352A             //HX8352A +486 bytes, 0.27s
-//#define SUPPORT_9326_5420         //ILI9326, SPFD5420 +232 bytes
-#define SUPPORT_B509_7793         //R61509, ST7793 +238 bytes
+//#define SUPPORT_9326_5420         //ILI9326, SPFD5420 +246 bytes
+#define SUPPORT_B509_7793         //R61509, ST7793 +244 bytes
 #define OFFSET_9327 32            //costs about 103 bytes, 0.08s
 
 #include "MCUFRIEND_kbv.h"
