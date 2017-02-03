a9f2b598d98659022684b47fb217637b2ce3fcbc
prenticedavid <david.prentice@farming.co.uk>
2017-02-03 22:38:50 +0000

    add ILI9340 as ILI9341 clone

--- MCUFRIEND_kbv.cpp
+++ MCUFRIEND_kbv.cpp
@@ -1643,7 +1643,7 @@ case 0x4532:    // thanks Leodino
         *p16 = 320;
         break;
 
-    case 0x8230:
+    case 0x8230:    //thanks Auzman
         _lcd_capable = 0 | REV_SCREEN | INVERT_GS | INVERT_RGB;
         goto common_9320;
     case 0x5408:
@@ -1903,7 +1903,8 @@ case 0x4532:    // thanks Leodino
         };
         table8_ads = ILI9329_regValues, table_size = sizeof(ILI9329_regValues);
         break;
-    case 0x9341:
+    case 0x9340:
+	case 0x9341:
         _lcd_capable = AUTO_READINC | MIPI_DCS_REV1 | MV_AXIS | READ_24BITS;
         static const uint8_t ILI9341_regValues_2_4[] PROGMEM = {        // BOE 2.4"
             0xF6, 3, 0x01, 0x01, 0x00,  //Interface Control needs EXTC=1 MV_EOR=0, TM=0, RIM=0
