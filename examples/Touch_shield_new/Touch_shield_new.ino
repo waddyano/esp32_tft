a8f854160c04861b2583e826309ccc4d71d6c374
prenticedavid <david.prentice@farming.co.uk>
2018-04-30 18:30:58 +0100

    remove old Touch examples, add Touch_shield_new

--- examples/Touch_shield_new/Touch_shield_new.ino
+++ examples/Touch_shield_new/Touch_shield_new.ino
@@ -2,57 +2,25 @@
 
 // different mcufriend shields have Touchscreen on different pins
 // and rotation.
-// Run the UTouch_calibr_kbv sketch for calibration of your shield
+// Run the TouchScreen_Calibr_native sketch for calibration of your shield
 
-#include <Adafruit_GFX.h>    // Core graphics library
-//#include <Adafruit_TFTLCD.h> // Hardware-specific library
-//Adafruit_TFTLCD tft(A3, A2, A1, A0, A4);
 #include <MCUFRIEND_kbv.h>
 MCUFRIEND_kbv tft;       // hard-wired for UNO shields anyway.
 #include <TouchScreen.h>
 
-#if defined(__SAM3X8E__)
-#undef __FlashStringHelper::F(string_literal)
-#define F(string_literal) string_literal
-#endif
+char *name = "Please Calibrate.";  //edit name of shield
+const int XP=6,XM=A2,YP=A1,YM=7; //ID=0x9341
+const int TS_LEFT=907,TS_RT=136,TS_TOP=942,TS_BOT=139;
 
-// These are the pins for some typical shields!
-// S6D0154: YP=A1, XM=A2, YM=7, XP=6
-// ST7783 : YP=A2, XM=A1, YM=6, XP=7
-// ILI9320: YP=A2, XM=A3, YM=8, XP=9
-// ILI9325: YP=A1, XM=A2, YM=7, XP=6
-// ILI9325BG: YP=A2, XM=A1, YM=6, XP=7
-// ILI9341: YP=A2, XM=A1, YM=7, XP=6
-// ILI9488: YP=A1, XM=A2, YM=7, XP=6
-// R65109V: YP=A2, XM=A1, YM=6, XP=7
-
-// most mcufriend shields use these pins and Portrait mode:
-uint8_t YP = A1;  // must be an analog pin, use "An" notation!
-uint8_t XM = A2;  // must be an analog pin, use "An" notation!
-uint8_t YM = 7;   // can be a digital pin
-uint8_t XP = 6;   // can be a digital pin
-uint8_t SwapXY = 0;
-
-uint16_t TS_LEFT = 920;
-uint16_t TS_RT  = 150;
-uint16_t TS_TOP = 940;
-uint16_t TS_BOT = 120;
-char *name = "Unknown controller";
-
-// For better pressure precision, we need to know the resistance
-// between X+ and X- Use any multimeter to read it
-// For the one we're using, its 300 ohms across the X plate
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 TSPoint tp;
 
-#define MINPRESSURE 20
+#define MINPRESSURE 200
 #define MAXPRESSURE 1000
 
-#define SWAP(a, b) {uint16_t tmp = a; a = b; b = tmp;}
-
 int16_t BOXSIZE;
-int16_t PENRADIUS = 3;
-uint16_t identifier, oldcolor, currentcolor;
+int16_t PENRADIUS = 1;
+uint16_t ID, oldcolor, currentcolor;
 uint8_t Orientation = 0;    //PORTRAIT
 
 // Assign human-readable names to some common 16-bit color values:
@@ -67,17 +35,19 @@ uint8_t Orientation = 0;    //PORTRAIT
 
 void show_Serial(void)
 {
-    Serial.print(F("Found "));
-    Serial.print(name);
-    Serial.println(F(" LCD driver"));
+    Serial.println(F("Most Touch Screens use pins 6, 7, A1, A2"));
+    Serial.println(F("But they can be in ANY order"));
+    Serial.println(F("e.g. right to left or bottom to top"));
+    Serial.println(F("or wrong direction"));
+    Serial.println(F("Edit name and calibration statements\n"));
+    Serial.println(name);
     Serial.print(F("ID=0x"));
-    Serial.println(identifier, HEX);
+    Serial.println(ID, HEX);
     Serial.println("Screen is " + String(tft.width()) + "x" + String(tft.height()));
     Serial.println("Calibration is: ");
     Serial.println("LEFT = " + String(TS_LEFT) + " RT  = " + String(TS_RT));
     Serial.println("TOP  = " + String(TS_TOP)  + " BOT = " + String(TS_BOT));
-    Serial.print("Wiring is: ");
-    Serial.println(SwapXY ? "SWAPXY" : "PORTRAIT");
+    Serial.println("Wiring is always PORTRAIT");
     Serial.println("YP=" + String(YP)  + " XM=" + String(XM));
     Serial.println("YM=" + String(YM)  + " XP=" + String(XP));
 }
@@ -85,23 +55,18 @@ void show_Serial(void)
 void show_tft(void)
 {
     tft.setCursor(0, 0);
-    tft.setTextSize(2);
-    tft.print(F("Found "));
-    tft.print(name);
-    tft.println(F(" LCD"));
     tft.setTextSize(1);
     tft.print(F("ID=0x"));
-    tft.println(identifier, HEX);
+    tft.println(ID, HEX);
     tft.println("Screen is " + String(tft.width()) + "x" + String(tft.height()));
-    tft.println("Calibration is: ");
+    tft.println("");
+    tft.setTextSize(2);
+    tft.println(name);
+    tft.setTextSize(1);
+    tft.println("PORTRAIT Values:");
     tft.println("LEFT = " + String(TS_LEFT) + " RT  = " + String(TS_RT));
     tft.println("TOP  = " + String(TS_TOP)  + " BOT = " + String(TS_BOT));
-    tft.print("\nWiring is: ");
-    if (SwapXY) {
-        tft.setTextColor(CYAN);
-        tft.setTextSize(2);
-    }
-    tft.println(SwapXY ? "SWAPXY" : "PORTRAIT");
+    tft.println("\nWiring is: ");
     tft.println("YP=" + String(YP)  + " XM=" + String(XM));
     tft.println("YM=" + String(YM)  + " XP=" + String(XP));
     tft.setTextSize(2);
@@ -115,8 +80,6 @@ void show_tft(void)
         tp = ts.getPoint();
         pinMode(XM, OUTPUT);
         pinMode(YP, OUTPUT);
-        pinMode(XP, OUTPUT);
-        pinMode(YM, OUTPUT);
         if (tp.z < MINPRESSURE || tp.z > MAXPRESSURE) continue;
         if (tp.x > 450 && tp.x < 570  && tp.y > 450 && tp.y < 570) break;
         tft.setCursor(0, (tft.height() * 3) / 4);
@@ -128,80 +91,11 @@ void show_tft(void)
 void setup(void)
 {
     uint16_t tmp;
-    Serial.begin(9600);
 
     tft.reset();
-    identifier = tft.readID();
-    //    if (identifier == 0) identifier = 0x9341;
-    if (0) {
-    } else if (identifier == 0x0154) {
-        name = "S6D0154";
-        TS_LEFT = 914; TS_RT = 181; TS_TOP = 957; TS_BOT = 208;
-    } else if (identifier == 0x5408) {  //thanks gazialankus
-        name = "SPFD5408";
-        TS_LEFT = 150; TS_RT = 960; TS_TOP = 155; TS_BOT = 925;
-        SwapXY = 1;
-    } else if (identifier == 0x7783) {
-        name = "ST7781";
-        TS_LEFT = 865; TS_RT = 155; TS_TOP = 942; TS_BOT = 153;
-        SwapXY = 1;
-    } else if (identifier == 0x7789) {
-        name = "ST7789V";
-        YP = A2; XM = A1; YM = 7; XP = 6;
-        TS_LEFT = 906; TS_RT = 169; TS_TOP = 161; TS_BOT = 919;
-    } else if (identifier == 0x9320) {
-        name = "ILI9320";
-        YP = A3; XM = A2; YM = 9; XP = 8;
-        TS_LEFT = 902; TS_RT = 137; TS_TOP = 941; TS_BOT = 134;
-    } else if (identifier == 0x9325) {
-        name = "ILI9325";
-        TS_LEFT = 900; TS_RT = 103; TS_TOP = 96; TS_BOT = 904;
-    } else if (identifier == 0x9325) {
-        name = "ILI9325 Green Dog";
-        TS_LEFT = 900; TS_RT = 130; TS_TOP = 940; TS_BOT = 130;
-    } else if (identifier == 0x9327) {
-        name = "ILI9327";
-        TS_LEFT = 899; TS_RT = 135; TS_TOP = 935; TS_BOT = 79;
-        SwapXY = 1;
-    } else if (identifier == 0x9329) {
-        name = "ILI9329";
-        TS_LEFT = 143; TS_RT = 885; TS_TOP = 941; TS_BOT = 131;
-        SwapXY = 1;
-    } else if (identifier == 0x9341) {
-        name = "ILI9341 BLUE";
-        TS_LEFT = 920; TS_RT = 139; TS_TOP = 944; TS_BOT = 150;
-        SwapXY = 0;
-    } else if (identifier == 0) {
-        name = "ILI9341 DealExtreme";
-        TS_LEFT = 893; TS_RT = 145; TS_TOP = 930; TS_BOT = 135;
-        SwapXY = 1;
-    } else if (identifier == 0 || identifier == 0x9341) {
-        name = "ILI9341";
-        TS_LEFT = 128; TS_RT = 911; TS_TOP = 105; TS_BOT = 908;
-        SwapXY = 1;
-    } else if (identifier == 0x9486) {
-        name = "ILI9486";
-        TS_LEFT = 904; TS_RT = 170; TS_TOP = 950; TS_BOT = 158;
-    } else if (identifier == 0x9488) {
-        name = "ILI9488";
-        TS_LEFT = 904; TS_RT = 170; TS_TOP = 950; TS_BOT = 158;
-    } else if (identifier == 0xB509) {
-        name = "R61509V";
-        TS_LEFT = 889; TS_RT = 149; TS_TOP = 106; TS_BOT = 975;
-        SwapXY = 1;
-    } else {
-        name = "unknown";
-    }
-    switch (Orientation) {      // adjust for different aspects
-        case 0:   break;        //no change,  calibrated for PORTRAIT
-        case 1:   tmp = TS_LEFT, TS_LEFT = TS_BOT, TS_BOT = TS_RT, TS_RT = TS_TOP, TS_TOP = tmp;  break;
-        case 2:   SWAP(TS_LEFT, TS_RT);  SWAP(TS_TOP, TS_BOT); break;
-        case 3:   tmp = TS_LEFT, TS_LEFT = TS_TOP, TS_TOP = TS_RT, TS_RT = TS_BOT, TS_BOT = tmp;  break;
-    }
-
+    ID = tft.readID();
+    tft.begin(ID);
     Serial.begin(9600);
-    ts = TouchScreen(XP, YP, XM, YM, 300);     //call the constructor AGAIN with new values.
-    tft.begin(identifier);
     show_Serial();
     tft.setRotation(Orientation);
     tft.fillScreen(BLACK);
@@ -230,22 +124,34 @@ void loop()
     // if sharing pins, you'll need to fix the directions of the touchscreen pins
     pinMode(XM, OUTPUT);
     pinMode(YP, OUTPUT);
-    pinMode(XP, OUTPUT);
-    pinMode(YM, OUTPUT);
-    //    digitalWrite(XM, HIGH);
-    //    digitalWrite(YP, HIGH);
     // we have some minimum pressure we consider 'valid'
     // pressure of 0 means no pressing!
 
     if (tp.z > MINPRESSURE && tp.z < MAXPRESSURE) {
-        // is controller wired for Landscape ? or are we oriented in Landscape?
-        if (SwapXY != (Orientation & 1)) SWAP(tp.x, tp.y);
-        // scale from 0->1023 to tft.width  i.e. left = 0, rt = width
         // most mcufriend have touch (with icons) that extends below the TFT
         // screens without icons need to reserve a space for "erase"
         // scale the ADC values from ts.getPoint() to screen values e.g. 0-239
-        xpos = map(tp.x, TS_LEFT, TS_RT, 0, tft.width());
-        ypos = map(tp.y, TS_TOP, TS_BOT, 0, tft.height());
+        //
+        // Calibration is true for PORTRAIT. tp.y is always long dimension 
+        // map to your current pixel orientation
+        switch (Orientation) {
+            case 0:
+                xpos = map(tp.x, TS_LEFT, TS_RT, 0, tft.width());
+                ypos = map(tp.y, TS_TOP, TS_BOT, 0, tft.height());
+                break;
+            case 1:
+                xpos = map(tp.y, TS_TOP, TS_BOT, 0, tft.width());
+                ypos = map(tp.x, TS_RT, TS_LEFT, 0, tft.height());
+                break;
+            case 2:
+                xpos = map(tp.x, TS_RT, TS_LEFT, 0, tft.width());
+                ypos = map(tp.y, TS_BOT, TS_TOP, 0, tft.height());
+                break;
+            case 3:
+                xpos = map(tp.y, TS_BOT, TS_TOP, 0, tft.width());
+                ypos = map(tp.y, TS_LEFT, TS_RT, 0, tft.height());
+                break;
+        }
 
         // are we in top color box area ?
         if (ypos < BOXSIZE) {               //draw white border on selected color box
@@ -285,6 +191,8 @@ void loop()
             tft.fillCircle(xpos, ypos, PENRADIUS, currentcolor);
         }
         // are we in erase area ?
+        // Plain Touch panels use bottom 10 pixels e.g. > h - 10
+        // Touch panels with icon area e.g. > h - 0
         if (ypos > tft.height() - 10) {
             // press the bottom of the screen to erase
             tft.fillRect(0, BOXSIZE, tft.width(), tft.height() - BOXSIZE, BLACK);
