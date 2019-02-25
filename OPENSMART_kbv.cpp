4aa663c9a3356e729a2714bc5ad1a4819e5d2306
prenticedavid <david.prentice@farming.co.uk>
2019-02-25 15:20:05 +0000

    enable HX852A, RM68130

--- OPENSMART_kbv.cpp
+++ OPENSMART_kbv.cpp
@@ -4,10 +4,14 @@
 
 #define USE_SPECIAL                     //SPECIAL will use the first match 
 //#define USE_OPENSMART_SHIELD_PINOUT     //deprecated
+//select the target board
 #define USE_OPENSMART_SHIELD_PINOUT_UNO
 #define USE_OPENSMART_SHIELD_PINOUT_MEGA
 #define USE_OPENSMART_SHIELD_PINOUT_DUE //thanks Michel53
-#define SUPPORT_8352B
+//select the target controllers that are not enabled by default 
+#define SUPPORT_8352A                   //HX8352A
+#define SUPPORT_8352B                   //HX8352B
+#define SUPPORT_9225                    //RM68130
 
 #include "OPENSMART_kbv.h"              //class declaration
 
